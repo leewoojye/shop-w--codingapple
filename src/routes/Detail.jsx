1ad0ebc4b93478fb47514b66b1963ce63d173850
WOOJYE <whitehole36@naver.com>
2024-11-02 23:24:04 +0000

    m

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -11,7 +11,14 @@ import {
   Col,
 } from "react-bootstrap";
 import { useState } from "react";
-import { Routes, Route, Link, useNavigate, Outlet, useParams } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet,
+  useParams,
+} from "react-router-dom";
 import data from "../data.js";
 // import styled from "styled-component";
 
@@ -38,47 +45,46 @@ function hasNonNumericCharacters(input) {
 }
 
 function Detail(props) {
+  // 유저가 URL 파라미터에 입력한 거 가져오려면
+  let { id } = useParams();
+  // let 찾은상품 = props.shoes.find(x=>x.id==id);
+  // URL 파라미터는 주로 React Router와 함께 사용되어 URL의 일부로 정보를 전달하는 방법이다. 이를 통해 특정 페이지나 컴포넌트에 필요한 데이터를 URL을 통해 전달할 수 있다.
 
-// 유저가 URL 파라미터에 입력한 거 가져오려면
-let {id}=useParams();
-// let 찾은상품 = props.shoes.find(x=>x.id==id);
-// URL 파라미터는 주로 React Router와 함께 사용되어 URL의 일부로 정보를 전달하는 방법이다. 이를 통해 특정 페이지나 컴포넌트에 필요한 데이터를 URL을 통해 전달할 수 있다.
-
-let [sales, salesChange] = useState(true);
-useEffect(()=>{
-  setTimeout(()=>{
-    salesChange(false);
-  }, 2000)
-}, [])
-let [input, inputChange] = useState('');
-let [warning, warningChange] = useState(false);
-useEffect(()=>{
-  if(hasNonNumericCharacters(input)){
-    warningChange(true);
-  } else {
-    warningChange(false);
-  }
-})
+  let [sales, salesChange] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      salesChange(false);
+    }, 2000);
+  }, []);
+  let [input, inputChange] = useState("");
+  let [warning, warningChange] = useState(false);
+  useEffect(() => {
+    if (hasNonNumericCharacters(input)) {
+      warningChange(true);
+    } else {
+      warningChange(false);
+    }
+  });
+  let [탭, 탭변경] = useState(0);
 
   return (
     <>
       <div className="container">
-        {
-          sales ? <div className="alert alert -warning">2초이내 구매시 할인</div> : <div></div>
-        }
+        {sales ? (
+          <div className="alert alert -warning">2초이내 구매시 할인</div>
+        ) : (
+          <div></div>
+        )}
         <div className="row">
           <div className="col-md-6">
-            <img
-              src={data[id].url}
-              width="100%"
-            />
+            <img src={data[id].url} width="100%" />
           </div>
-          {
-            warning ? <div>그러지 마세요</div> : null
-          }
-          <input onChange={(e)=>{
-            inputChange(e.target.value)
-          }}/>
+          {warning ? <div>그러지 마세요</div> : null}
+          <input
+            onChange={(e) => {
+              inputChange(e.target.value);
+            }}
+          />
           <div className="col-md-6">
             <h4 className="pt-5">{data[id].title}</h4>
             <p>{data[id].content}</p>
@@ -86,9 +92,63 @@ useEffect(()=>{
             <button className="btn btn-danger">주문하기</button>
           </div>
         </div>
+
+        <Nav variant="tabs" defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link0"
+              onClick={() => {
+                탭변경(0);
+              }}
+            >
+              버튼0
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link1"
+              onClick={() => {
+                탭변경(1);
+              }}
+            >
+              버튼1
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link2"
+              onClick={() => {
+                탭변경(2);
+              }}
+            >
+              버튼2
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent 탭={탭} />
       </div>
     </>
   );
 }
+
+function TabContent({ 탭 }) {
+  // if 조건문 대신 배열로 분기 가능
+  let [fade, setfade] = useState("");
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setfade("end");
+    }, 100);
+    return () => {
+      clearTimeout(a);
+      setfade("");
+    };
+  }, [탭]); // 탭 state가 변할 때마다 end를 부착하도록 작동
+  return (
+    <div className={`start ${fade}`}>
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭]}
+    </div>
+  );
+}
+// JSX 내 삼항연산자는 한개밖에, 조건문은 아예 못쓴다고함
 
 export default Detail;
